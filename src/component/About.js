7ed2329d330729662ce946624603f7b16e7d95d4
Schleidens <alectineschleidens2016@gmail.com>
2022-11-16 14:49:02 -0500

    add About page and design it/ make the router work/ clean the css code

---
+++ src/component/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+        <div className="about__box">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut recusandae necessitatibus, molestiae quo libero, aspernatur unde dolore earum fugiat eaque doloremque error quaerat debitis, rerum officia hic sint eius ullam.
+        </div>
+    </>
+  )
+}
+
+export default About
