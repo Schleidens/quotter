e6e1a1bb03be76008c8d6ac80c85f2607da5b0e0
Schleidens <alectineschleidens2016@gmail.com>
2022-11-18 10:42:41 -0500

    adding real quote in the DB

--- src/db/quoteDb.js
+++ src/db/quoteDb.js
@@ -1,19 +1,53 @@
 const quotes = [
     {
-        "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-        "author": "jane Doe"
+        "quote": "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+        "author": "Martin Fowler "
     },
     {
-        "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-        "author": "John e"
+        "quote": "First, solve the problem. Then, write the code.",
+        "author": "John Johnson"
     },
     {
-        "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-        "author": "John D"
-    },{
-        "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        "quote": "Experience is the name everyone gives to their mistakes.",
+        "author": "Chris Heilmann"
+    },
+    {
+        "quote": "Java is to JavaScript what car is to Carpet.",
         "author": "John "
+    },
+    {
+        "quote": "Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code.",
+        "author": "Dan Salomon "
+    },
+    {
+        "quote": "Ruby is rubbish! PHP is phpantastic!",
+        "author": "Nikita Popov"
+    },
+    {
+        "quote": "Code is like humor. When you have to explain it, it’s bad.",
+        "author": "Cory House"
+    },
+    {
+        "quote": "Fix the cause, not the symptom.",
+        "author": "Steve maguire"
+    },
+    {
+        "quote": "Optimism is an occupational hazard of programming: feedback is the treatment. ",
+        "author": "Kent Beck"
+    },
+    {
+        "quote": "When to use iterative development? You should use iterative development only on projects that you want to succeed.",
+        "author": "Martin Fowler"
+    },
+    {
+        "quote": "Before software can be reusable it first has to be usable.",
+        "author": "Ralph Johnson"
+    },
+    {
+        "quote": "Make it work, make it right, make it fast.",
+        "author": "Kent Beck"
     }
+
 ]
 
 export default quotes;
