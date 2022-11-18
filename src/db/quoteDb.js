bdb54350283406799b17b5e98c82b5f5b1755d8f
Schleidens <alectineschleidens2016@gmail.com>
2022-11-18 10:28:33 -0500

    mock the quoteDb to get the quote

---
+++ src/db/quoteDb.js
@@ -0,0 +1,19 @@
+const quotes = [
+    {
+        "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        "author": "jane Doe"
+    },
+    {
+        "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        "author": "John e"
+    },
+    {
+        "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        "author": "John D"
+    },{
+        "quote": "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        "author": "John "
+    }
+]
+
+export default quotes;
